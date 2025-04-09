4f6d14aa7891ebe1288e06990f927b5308a983b2
dhananjaisaxena0911 <dhananjaisaxena@gmail.com>
2025-04-09 15:13:14 +0530

    Revert "implemented LoginSignup Controllers Plus Routers"

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./configs/db.js";
-import authRoutes from "../backend/routers/authRoutes.js";
 
 dotenv.config();
 
@@ -11,5 +10,3 @@ app.listen(process.env.PORT, () => {
   connectDB();
   console.log(`Server is running on port ${process.env.PORT}`);
 });
-
-app.use('/auth',authRoutes);
