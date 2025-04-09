54d82eeb761c4ee64a1c0f562666b5c6c0e4afaa
Prateekshit <prateekshitwork@gmail.com>
2025-04-09 15:37:17 +0530

    Adjust Login icon

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React from "react";
 import { SiGnuprivacyguard } from "react-icons/si";
 import { PiStudentFill } from "react-icons/pi";
+import { CiLogin } from "react-icons/ci";
 
 function NavBar() {
   return (
     <div>
-      <div className='flex text-2xl bg-gray-800 rounded-lg m-5 p-3 justify-between'>
-        <a href="/" className='font-bold text-white text-center'><PiStudentFill/></a>
-              <a href="/" className='font-bold text-white text-center'>Student Management System</a>
-              <a href="/signup" className='text-2xl text-white hover:text-blue'><SiGnuprivacyguard/></a>
+      <div className="flex text-2xl bg-gray-800 rounded-lg m-5 p-3 items-center justify-between">
+        {/* Left icon */}
+        <a href="/" className="text-white">
+          <PiStudentFill />
+        </a>
+
+        {/* Centered text */}
+        <a href="/" className="font-bold text-white text-center flex-1">
+          Student Management System
+        </a>
+
+        {/* Right icons */}
+        <a href="/signup" className="text-white hover:text-blue-500 px-3">
+          <SiGnuprivacyguard />
+        </a>
+        <a href="/login" className="text-white hover:text-blue-500">
+          <CiLogin />
+        </a>
       </div>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
