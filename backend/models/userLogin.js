01e46e31cb6af3fc57b420b99995472035ce55ff
prateekshit-v <prateekshitwork@gmail.com>
2025-04-09 17:17:25 +0530

    Revert "dhanan09 made controller and routes"

--- backend/models/userLogin.js
+++ backend/models/userLogin.js
@@ -1,28 +1,30 @@
-import mongoose from 'mongoose';
-const validateEmail = function (email) {
-    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    return re.test(email);
-};
-const userLoginSchema = new mongoose.Schema({
-    Name: {
-        type: String,
-        required: true,
-        maxLength: 20,
-    },
-    Email: {
-        type: String,
-        trim: true,
-        lowercase: true,
-        unique: true,
-        required: "Email address is required",
-        validate: [validateEmail, "Please fill a valid email Address!"],
-    },
-    Password: {
-        type: String,
-        required: true,
+const mongoose= require("mongoose");
+
+const userLoginSchema= new mongoose.Schema(
+    {
+        Name:{
+            type:String,
+            required:true,
+            maxLength:20,
+        },
+        Email:{
+            type:String,
+            trim:true,
+            lowercase:true,
+            unique:true,
+            required:"Email address is required",
+            validate:[validateEmail,"Please fill a valid email Address!"],
+        },
+        Password:{
+            type:String,
+            required:true,
+        }
     }
-});
+);
 
-const UserModel = mongoose.model("UserModel", userLoginSchema);
+var validateEmail= function(email){
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
 
-export default UserModel;
+module.exports= mongoose.model("UserModel",userLoginSchema);
