d4a8a2b2f0a01a341583f761ef5ec0ce3006e2f3
dhananjaiSaxena <dhananjaisaxena@gail.com>
2025-04-09 12:53:33 +0530

    implemented studentModelSchema

---
+++ backend/models/student.model.js
@@ -0,0 +1,23 @@
+const mongoose= require("mongoose");
+const studentDataSchema=new mongoose.Schema(
+    {
+        Name:{
+            type:String,
+            required:true,
+            MaxLength:20,
+        },
+        Class:{
+            type:Number,
+            required:true,
+        },
+        Age:{
+            type:Number,
+            required:true,
+        },
+        Course:{
+            type:String,
+            required:true
+        }
+    }
+)
+module.exports= mongoose.model("studentModel",studentDataSchema);
